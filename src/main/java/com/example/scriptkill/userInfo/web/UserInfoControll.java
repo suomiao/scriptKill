4a62018860a8913678d95108f64dfcf7442690c1
1351281000@qq.com <1351281000@qq.com>
2021-12-26 21:02:18 +0800

    添加了日志打印

--- src/main/java/com/example/scriptkill/userInfo/web/UserInfoControll.java
+++ src/main/java/com/example/scriptkill/userInfo/web/UserInfoControll.java
@@ -2,6 +2,10 @@ package com.example.scriptkill.userInfo.web;
 
 import com.example.scriptkill.userInfo.entity.UserInfo;
 import com.example.scriptkill.userInfo.service.UserInfoService;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @RestController
 @RequestMapping("/userInfo")
 public class UserInfoControll {
 
+    private static final Logger log = LoggerFactory.getLogger(UserInfoControll.class);
+//    Logger log = LoggerFactory.getLogger(UserInfoControll.class);
     @Autowired
     UserInfoService userInfoService;
 
@@ -24,7 +31,8 @@ public class UserInfoControll {
 
     @RequestMapping("getById")
     public UserInfo getById(HttpServletRequest request, HttpServletResponse response) {
-//        String userId = request.getParameter("userId");
+        String userId = request.getParameter("userId");
+        log.info("getById" + "参数获取：" + userId);
         UserInfo userInfo = userInfoService.get(1);
         return userInfo;
     }
