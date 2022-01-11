2928bbc3bd7a7e1bc5756888677f602997ac4af9
suomiao <1351281000@qq.com>
2022-01-11 09:24:19 +0800

    yjj: 添加

---
+++ src/test/java/com/example/scriptkill/userinfo/web/UserInfoControllTest.java
@@ -0,0 +1,14 @@
+package com.example.scriptkill.userinfo.web;
+
+import org.junit.jupiter.api.Test;
+
+class UserInfoControllTest {
+
+    @Test
+    void say() {
+    }
+
+    @Test
+    void getById() {
+    }
+}
